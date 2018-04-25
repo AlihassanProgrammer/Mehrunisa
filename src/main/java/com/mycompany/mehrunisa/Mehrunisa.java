099efeefe0cfb207e22a99c49820ef838d92ef45
Ali hassan <11745671+AlihassanProgrammer@users.noreply.github.com>
2018-04-25 15:18:49 +0500

    Issue Resolve

--- src/main/java/com/mycompany/mehrunisa/Mehrunisa.java
+++ src/main/java/com/mycompany/mehrunisa/Mehrunisa.java
@@ -9,28 +9,32 @@ import com.sun.speech.freetts.*;
  *
  * @author Ali Hassan
  */
-public class Mehrunisa {
-
+public class Mehrunisa { 
+    
     private static final String VOICENAME_kevin = "kevin";
-        private String text; // string to speech
-        public void speak() {
-            Voice voice;
-            VoiceManager voiceManager = VoiceManager.getInstance();
-            voice = voiceManager.getVoice(VOICENAME_kevin);
-            voice.allocate();
-            voice.speak(text);
-        }
-
-        public Mehrunisa(String text) {
+    private String text; // string to speech
+    public Mehrunisa(String text) {
         this.text = text;
         }        
+    public void speak() throws Exception{
+             Voice voice;
+            VoiceManager voiceManager = VoiceManager.getInstance();
+               voice = voiceManager.getVoice(VOICENAME_kevin);    
+                System.out.println("Voice :" + voice);
+                voice.allocate();
+                voice.speak(text);
+        }
+
+            
     /**
      * @param args the command line arguments
+     * @throws java.lang.Exception
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         // TODO code application logic here
-          String text = "Hello ";
-         Mehrunisa mehrunisa = new Mehrunisa(text);
+        System.setProperty("freetts.voices", "com.sun.speech.freetts.en.us.cmu_us_kal.KevinVoiceDirectory");
+        String message = "Hello world! This is a test program";
+         Mehrunisa mehrunisa = new Mehrunisa(message);
          mehrunisa.speak();
     }
     
